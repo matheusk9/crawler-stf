8b7d091bb64194e34edce0407d1d91c534c219bb
Matheus Freitas <matheus.freitas@justicafacil.com.br>
2023-05-04 17:36:18 -0300

    del function
    remocao de polimorfismo desnecessario

--- hashcode.py
+++ hashcode.py
@@ -5,18 +5,14 @@ import requests
 from crawler import Crawler
 
 
-class HashCode(Crawler):
+class HashCode:
     """docstriasdasd"""
 
-    def requisicao(self, link: str, config: dict, time=60):
-        """doc"""
-        response = requests.get(link, headers=config, timeout=time)
-        return response
-
     @staticmethod
-    def generate(dicionario: dict, link, conf):
+    def generate(dicionario: dict, link, obj: Crawler):
         """docstring"""
-        response = HashCode.requisicao(HashCode, link, conf)
+
+        response = requests.get(url=link, headers=obj.get_user_agent(), timeout=60)
         pdf_content = response.content
         md5_hash = hashlib.md5(pdf_content).hexdigest()
         dicionario[md5_hash] = link
