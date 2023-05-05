ceb001d55b57985d733b3543fe7650486bb23b66
Matheus Freitas <matheus.freitas@justicafacil.com.br>
2023-05-04 21:00:25 -0300

    add documentação

--- crawler.py
+++ crawler.py
@@ -1,25 +1,36 @@
-"""doc"""
+"""Modulo de requisições."""
 import requests
 from bs4 import BeautifulSoup
 
 
-class Crawler():
-    """docstriasdasd"""
+class Crawler:
+    """Responsavel pela configuração e requests."""
 
     def __init__(self):
-        self.data = '13-12-2022'
+        self.data = "13-12-2022"
         self._user_agent = {
-            "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+            "User-agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/89.0.4389.90 Safari/537.36"
+            )
         }
-        self._url = f"https://portal.stf.jus.br/servicos/dje/listarDiarioJustica.asp?tipoVisualizaDJ=periodoDJ&txtNumeroDJ=&txtAnoDJ=2022&dataInicial={self.data}&dataFinal={self.data}&tipoPesquisaDJ=&argumento="
+        self._url = (
+            "https://portal.stf.jus.br/servicos/dje/listarDiarioJustica.asp?"
+            "tipoVisualizaDJ=periodoDJ&txtNumeroDJ=&txtAnoDJ=2022&"
+            f"dataInicial={self.data}&dataFinal={self.data}&tipoPesquisaDJ=&argumento="
+        )
 
     @property
     def user_agent(self):
-        """doc"""
+        """Metodo get."""
         return self._user_agent
 
     def requisicao(self, link=None, time=60):
-        """doc"""
+        """Faz a requisicao do link passado por parametro.
+
+        Response = pega o HTML bruto.
+        Soup = analisa e processa o HTML.
+        """
 
         if link is None:
             link = self._url
@@ -28,27 +39,30 @@ class Crawler():
         return soup
 
     def get_url_acesso(self):
-        """doc"""
+        """Acesso a primeira pagina.
 
-        # page 1 - LISTAS
+        Faz o request da primeira pagina e busca todo link na lista 'ul'.
+        Armazena todos os links na lista 'url'.
+        """
+
         lista_pdf = self.requisicao().find(
             "ul", {"class": "result__container--simples"}
         )
         url = []
-        # pega cada link de cada item que aparece na page 1
         for item in lista_pdf:
             links_dj = item.find("a")["href"]
             url.append("https://portal.stf.jus.br/servicos/dje/" + str(links_dj))
         return url
 
     def get_url_integral(self, url: list):
-        """doc"""
+        """Acesso a pagina de PDFs integrais e paginados.
+
+        Busca apenas links de PDFs integrais e armazena na lista pdf_url.
+        """
 
         pdf_url = []
-        # page 2 (integral e paginado)
         for link in url:
             integ_pag = self.requisicao(link).find_all("a", {"target": "_blank"})
-            # page 3 - pegando link apenas dos PDFs integrais
             for pdf_link in integ_pag:
                 if "Integral" in pdf_link.text:
                     pdf_url.append("https://portal.stf.jus.br" + str(pdf_link["href"]))
