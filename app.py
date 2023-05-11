8218dba73b1c79221cf5cbe112c6bdacf73d8736
Matheus Freitas <matheus.freitas@justicafacil.com.br>
2023-05-11 12:22:39 -0300

    Chama metodo run

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 """Módulo de execução e instâncias."""
 from src.modules.crawler import Crawler
 
-extracao = Crawler('02-11-2022')
-
-pdf_integral = extracao.obtem_url_integral()
-
-for url in pdf_integral:
-    extracao.gera_hashcode(url)
-print(extracao.dicionario.items())
+Crawler.run('02-12-2021')
